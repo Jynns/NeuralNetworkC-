9b73408f69201eadf514c6749653cdb6c3c7b3bc
Jynns <jannisarnold1@gmail.com>
2020-03-02 21:39:19 +0100

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "NeuronalesNetz.h"
+#include "NeuralNetwork.h"
 #include <math.h>
 
 int main() {
@@ -7,7 +7,7 @@ int main() {
   
 	std::vector<std::vector<float>> X{ { 0.1,.1 }, {.2,.2},{ .3,.3 }, {.4,.4},{ .5,.5 }, {.6,.6} };
 	std::vector<std::vector<float>> y{ {0.1},{0.2},{0.3},{0.4},{0.5},{0.6} };
-  //just small test if it fits by letting it overfit
+  	//just small test if it fits by letting it overfit
 	int epochs = 1000;
 	for (int i = 0; i < epochs; i++) {
 		testNet.train(X, y);
@@ -15,6 +15,6 @@ int main() {
 	
 	std::vector<float>Test{7, 7};
 	testNet.predict(Test);
-  std::cout << "Output " << testNet.getOutputLayer()[0].getValue() << std::endl;
+  	std::cout << "Output " << testNet.getOutputLayer()[0].getValue() << std::endl;
 	system("pause");
 }
