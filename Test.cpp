291386dc8233613e54ea1f2a993b3cdd18ae5fc0
Jynns <jannisarnold1@gmail.com>
2020-03-02 21:35:02 +0100

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "NeuronalesNetz.h"
+#include <math.h>
+
+int main() {
+	NeuronalesNetz testNet = NeuronalesNetz(2, 1, 1, .3f);
+  
+	std::vector<std::vector<float>> X{ { 0.1,.1 }, {.2,.2},{ .3,.3 }, {.4,.4},{ .5,.5 }, {.6,.6} };
+	std::vector<std::vector<float>> y{ {0.1},{0.2},{0.3},{0.4},{0.5},{0.6} };
+  //just small test if it fits by letting it overfit
+	int epochs = 1000;
+	for (int i = 0; i < epochs; i++) {
+		testNet.train(X, y);
+	}
+	
+	std::vector<float>Test{7, 7};
+	testNet.predict(Test);
+  std::cout << "Output " << testNet.getOutputLayer()[0].getValue() << std::endl;
+	system("pause");
+}
